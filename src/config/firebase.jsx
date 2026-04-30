02e5668e802ba4edba240f549e93d6034a650f2b
ChocoFantasy <angel861107@gmail.com>
2026-04-30 14:20:46 +0800

    Update firebase.jsx

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -4,15 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // 匯入firebase
-import{getAuth, GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyB61_eieiE_aQmE7dumMI54LmXi96mzscg",
+  authDomain: "mystic-markers-80316.firebaseapp.com",
+  projectId: "mystic-markers-80316",
+  storageBucket: "mystic-markers-80316.firebasestorage.app",
+  messagingSenderId: "823621529756",
+  appId: "1:823621529756:web:cfc4d637a3d85be1f73d59",
+  measurementId: "G-5V1JSM1GBX"
 };
 
 // Initialize Firebase
