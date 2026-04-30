db738d37edb391168a3f27c7f67786da03c98316
ChocoFantasy <angel861107@gmail.com>
2026-04-30 14:46:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Vercel 部署用根路徑，GitHub Pages 需要加上 repo 名稱
-  base: process.env.VERCEL ? '/' : (mode === 'production' ? '/Mystic-Markers/' : '/'),
+  base: process.env.VERCEL ? '/' : (mode === 'production' ? '/Mystic-Markers-/' : '/'),
 
   plugins: [react()],
 
